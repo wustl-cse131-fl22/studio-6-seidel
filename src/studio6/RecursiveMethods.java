e9d0c11a29daced163b19f14477af438f162296b
Sophie Seidel <s.m.seidel@wustl.edu>
2022-11-01 19:51:15 -0500

    conti

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,11 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			if(n==0) {
+				return 0;
+			}else {
+				return Math.pow(0.5, n) + geometricSum(n-1);
+			}
 		
 	}
 
@@ -29,7 +33,11 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+			if(q == 0) {
+				return p;
+			} else {
+				return gcd(q, (p%q));
+			}
 		
 	}
 
